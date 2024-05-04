5028ef5b66d03989de9a172560c585f2d88b450b
sebastian mendez <103006900+sebaditas@users.noreply.github.com>
2024-05-04 05:02:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const cors = require("cors");
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Cors configuration - Allows requests from localhost:4200
 const corsOptions = {
